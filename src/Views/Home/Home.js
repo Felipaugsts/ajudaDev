6a61fa568ed2775acc3cc31b790c9c8c5a75283c
Felipe Augusto Silva <felipeas@ciant.com>
2022-03-21 16:18:22 -0300

    feat: starter files

---
+++ src/Views/Home/Home.js
@@ -0,0 +1,16 @@
+
+import './Home.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>
+         Home
+       </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
