1b737899e99334cef4b96d6846290f324114587a
Felipe Augusto Silva <felipeas@ciant.com>
2022-03-22 14:16:45 -0300

    feat: adding loader

--- src/Views/Home/HomeController.js
+++ src/Views/Home/HomeController.js
@@ -2,23 +2,47 @@ import React, { Component } from 'react'
 import HomeList from './HomeList'
 import './Home.css'
 import api from '../../Model/API.js'
+import loader from '../../Assets/images/loader.gif'
 
 class HomeController extends Component {
+    constructor() { 
+        super()
+
+        this.state = { 
+            jobs: []
+        }
+    }
+    fetchJobs = () => { 
+        api.FetchJobs()
+        .then(job => this.setState({ 
+            jobs: job
+        }))
+    
+    }
+    componentDidUpdate() { 
+        console.log(this.state.jobs)
+    }
 
     componentDidMount() { 
-        api.FetchJobs().then((res) => {
-            console.log(res)
-        })
+       this.fetchJobs()
     }
 
     render() { 
+       if (this.state.jobs.length == 0) {
+           console.log("loading")
+           return ( 
+               <img className='loader-image' src={loader} /> 
+         )
+       } else {
         return (
             <div className='container'>
                 <div className='text xlarge heavy '>Encontre seu primeiro emprego <br />
                     <span className='hashtag heavier'>#AjudaDev</span>
                 </div>
             <HomeList />
-            </div>)
+            </div>
+            )
+       }
     }
 }
 export default HomeController
